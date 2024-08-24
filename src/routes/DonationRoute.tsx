8592d2223c1b3119f32f2e7d39118c81307dfbeb
Yongwook Choi <flow@hrmm.xyz>
2024-08-24 19:07:15 +0900

    fix label content

--- src/routes/DonationRoute.tsx
+++ src/routes/DonationRoute.tsx
@@ -122,14 +122,13 @@ function Section0() {
           render={({ ref, cn, style }) => (
             <Typography.h3 ref={ref} style={style} className={cn()}>
               <div>
-                <p>후원해주신 발전기금은 아래와 같은 명목으로 사용됩니다.</p>
+                <p>"정보대 발전기금"으로 기부하시면 아래와 같은 명목으로 사용됩니다.</p>
                 <ul
                   className={css({
                     listStyleType: "disc",
                     listStylePosition: "inside",
                   })}
                 >
-                  <li>정보대 발전 기금</li>
                   <li>교육/연구 공간 확보 및 유지 보수 기금</li>
                   <li>우수 교수 유치 기금</li>
                   <li>학생 생활 및 활동 지원</li>
