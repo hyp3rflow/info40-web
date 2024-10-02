783408410a05d47885e26d2f394af5fb56bdcca6
Yongwook Choi <flow@hrmm.xyz>
2024-10-02 16:43:26 +0900

    donate update

--- src/data/donate.ts
+++ src/data/donate.ts
@@ -11,6 +11,7 @@ export const donates = [
   { name: "전진섭", title: "졸업생 (교우)", date: "9월 3일" },
   { name: "김정현, DXP Lab", title: "정보대학 학장", date: "9월 4일" },
   { name: "신동익", title: "졸업생 (교우)", date: "9월 10일" },
-  { name: "민병경", title: "교수", date: "9월 11일" },
+  { name: "민병경, Min Lab", title: "교수", date: "9월 11일" },
   { name: "정원주, 통계지능연구실", title: "교수", date: "9월 19일" },
+  { name: "이성환, 패턴인식및머신러닝연구실", title: "교수", date: "10월 2일" },
 ];
